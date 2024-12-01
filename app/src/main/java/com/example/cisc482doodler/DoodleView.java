c10e1ce2448224db066bf2514a95b80006109987
cstet23 <cstetler@udel.edu>
2024-12-01 17:11:42 -0500

    Basic button functionality - next is setters and custom changes

--- app/src/main/java/com/example/cisc482doodler/DoodleView.java
+++ app/src/main/java/com/example/cisc482doodler/DoodleView.java
@@ -11,6 +11,8 @@ import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import androidx.annotation.ColorInt;
+
 public class DoodleView extends View {
 
     private Bitmap b;
@@ -36,7 +38,6 @@ public class DoodleView extends View {
     @Override
     protected void onDraw(Canvas c) {
         super.onDraw(c);
-
         c.drawBitmap(b, m, null);
     }
 
@@ -53,5 +54,26 @@ public class DoodleView extends View {
             return true;
         }
         return false;
+    }
+
+    public @ColorInt int getColor() {
+        return p.getColor();
+    }
+
+    public void changeColor(@ColorInt int newColor) {
+        p.setColor(newColor);
+    }
+
+    public void changeSize(int newSize) {
+        p.setStrokeWidth(newSize);
+    }
+
+    public void changeOpacity(int newOpacity) {
+        p.setAlpha(newOpacity);
+    }
+
+    public void clearImage() {
+        b.eraseColor(Color.TRANSPARENT);
+        invalidate();
     }
 }
--- app/src/main/java/com/example/cisc482doodler/MainActivity.java
+++ app/src/main/java/com/example/cisc482doodler/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.cisc482doodler;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
+import static java.lang.Math.random;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -14,13 +13,13 @@ import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.BitmapKt;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +32,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
         DoodleView doodleview = findViewById(R.id.doodleView);
+
         ImageButton brushColor = (ImageButton) findViewById(R.id.brushColor);
+        brushColor.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                doodleview.changeColor(Color.RED);
+                brushColor.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                return true;
+            }
+        });
+
         ImageButton brushSize = (ImageButton) findViewById(R.id.brushSize);
+        brushSize.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                doodleview.changeSize(5);
+                return true;
+            }
+        });
+
         ImageButton brushOpacity = (ImageButton) findViewById(R.id.brushOpacity);
+        brushOpacity.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                doodleview.changeOpacity(100);
+                return true;
+            }
+        });
+
         ImageButton clearImage = (ImageButton) findViewById(R.id.clearImage);
+        clearImage.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                doodleview.clearImage();
+                return true;
+            }
+        });
     }
 }
